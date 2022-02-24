e687217657c482dba634d3f18695d59dca652ec9
Julius Piso <julius@piso.at>
2022-02-24 14:05:04 +0100

    Implemented custom KDTree

--- include/cmKDTree.hpp
+++ include/cmKDTree.hpp
@@ -1,112 +1,325 @@
 #include <algorithm>
 #include <array>
+#include <map>
+#include <memory>
+#include <optional>
+#include <utility>
 #include <vector>
 
-#include <lsMessage.hpp>
+#include <lsSmartPointer.hpp>
 
-template <class T, int D> class cmKDTree {
-  using PointType = std::array<T, 3>;
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
-  std::vector<PointType> points;
-  size_t N;
+int intLog2(int x) {
+  int val = 0;
+  while (x >>= 1)
+    ++val;
+  return val;
+}
 
-  std::array<std::vector<size_t>, D> indices;
-  size_t maxDepth = std::numeric_limits<size_t>::max();
+enum struct cmKDTreeDistanceEnum : unsigned { EUCLIDEAN = 0, CUSTOM = 1 };
 
+template <class T, int D, class CoordType> class cmKDTree {
+public:
+  using SizeType = std::size_t;
+  //using CoordType = std::array<T, D>;
+  using DistanceFunctionType = T (*)(const CoordType &, const CoordType &);
+
+private:
+  SizeType N;
+  SizeType treeSize = 0;
+
+  int numThreads = 1;
+  int maxParallelDepth = 0;
+  int surplusWorkers = 0;
+
+  lsSmartPointer<std::vector<CoordType>> points = nullptr;
+
+  cmKDTreeDistanceEnum distanceType = cmKDTreeDistanceEnum::EUCLIDEAN;
+  DistanceFunctionType customDistance = nullptr;
+  DistanceFunctionType customReducedDistance = nullptr;
+
+public:
   struct Node {
-    T value;
-    Node *leftChild = nullptr;
-    Node *rightChild = nullptr;
-    Node() {}
-    Node(T passedValue, Node *passedLeftChild, Node *passedRightChild)
-        : value(passedValue), rightChild(passedRightChild),
-          leftChild(passedLeftChild) {}
+    CoordType &value;
+    int axis;
+
+    Node *parent = nullptr;
+    Node *left = nullptr;
+    Node *right = nullptr;
+
+    Node(CoordType &passedValue, int passedAxis)
+        : value(passedValue), axis(passedAxis) {}
   };
 
+  /** For later use with kNN **/
+  // template <class N> struct BoundedMultimap {
+  //   using MapType = std::multimap<T, N>;
+
+  // private:
+  //   MapType mmap;
+
+  // public:
+  //   const SizeType k;
+
+  //   BoundedMultimap(SizeType passedK) : k(passedK) {}
+
+  //   typename MapType::iterator emplace(std::pair<T, N> &&item) {
+  //     auto iterator = mmap.emplace(std::forward(item));
+  //     if (mmap.size() > k)
+  //       mmap.erase(mmap.rbegin());
+
+  //     return iterator;
+  //   }
+
+  //   std::optional<N> getMinItem() {
+  //     auto item = mmap.begin();
+  //     if (item != nullptr) {
+  //       mmap.erase(mmap.begin());
+  //       return {item->second};
+  //     } else {
+  //       return {};
+  //     }
+  //   }
+
+  //   T min() const {
+  //     return mmap.empty() ? std::numeric_limits<T>::max() :
+  //     mmap.begin()->first;
+  //   }
+
+  //   T max() const {
+  //     return mmap.empty() ? std::numeric_limits<T>::max()
+  //                         : mmap.rbegin()->first;
+  //   }
+  // };
+
+private:
   Node *rootNode = nullptr;
 
-  int nextAxis(int axis) const { return (axis + 1) % D; }
+  void build(Node *parent, typename std::vector<CoordType>::iterator start,
+             typename std::vector<CoordType>::iterator end, int depth,
+             bool isLeft) {
+    SizeType size = end - start;
 
-  void recurse(Node *self, const std::vector<size_t> &indices, size_t depth,
-               int axis) {
-    if (indices.size() == 1)
-      return;
-    if (depth >= maxDepth)
-      return;
+    int axis = depth % D;
+    // TODO: Add bucket size parameter
+    if (size > 1) {
+      SizeType medianIndex = (size + 1) / 2 - 1;
+      std::nth_element(
+          start, start + medianIndex, end,
+          [&](CoordType &a, CoordType &b) { return a[axis] < b[axis]; });
 
-    std::sort(indices.begin(), indices.end(), [&](size_t a, size_t b) {
-      return points[a][axis] < points[b][axis];
-    });
+      Node *current = new Node(*(start + medianIndex), axis);
 
-    size_t median = indices.size() / 2;
+      if (isLeft)
+        parent->left = current;
+      else
+        parent->right = current;
 
-    Node *left = new Node();
-    Node *right = new Node();
+#ifdef _OPENMP
+      bool dontSpawnMoreThreads = depth > maxParallelDepth + 1 ||
+                                  (depth == maxParallelDepth + 1 &&
+                                   omp_get_thread_num() >= surplusWorkers);
+#endif
+#pragma omp task final(dontSpawnMoreThreads)
+      {
+#ifdef _OPENMP
+#ifndef NDEBUG
+        std::cout << "Task assigned to thread " << omp_get_thread_num()
+                  << std::endl;
+#endif
+#endif
+        // Left Subtree
+        build(current,             // Use current node as parent
+              start,               // Data start
+              start + medianIndex, // Data end
+              depth + 1,           // Depth
+              true                 // Left
+        );
+      }
 
-    int newAxis = nextAxis(axis);
-
-    recurse(left,
-            std::vector<size_t>(indices.begin(), indices.begin() + median),
-            depth + 1, newAxis);
-
-    recurse(right, std::vector<size_t>(indices.begin() + median, indices.end()),
-            depth + 1, newAxis);
-
-    self->value = points[median][axis];
-    self->leftChild = left;
-    self->rightChild = right;
+      //  Right Subtree
+      build(current,                 // Use current node as parent
+            start + medianIndex + 1, // Data start
+            end,                     // Data end
+            depth + 1,               // Depth
+            false                    // Right
+      );
+#pragma omp taskwait
+    } else if (size == 1) {
+      // Leaf Node
+      if (isLeft)
+        parent->left = new Node(*start, axis);
+      else
+        parent->right = new Node(*start, axis);
+    }
   }
 
   void recursiveFree(Node *node) {
-    if (node->leftChild != nullptr)
-      recursiveFree(node->leftChild);
-    if (node->rightChild != nullptr)
-      recursiveFree(node->rightChild);
-    free(node);
+    if (node != nullptr) {
+      if (node->left != nullptr)
+        recursiveFree(node->left);
+      if (node->right != nullptr)
+        recursiveFree(node->right);
+
+      delete node;
+    }
+  }
+
+  static T euclideanReducedDistance(const CoordType &a, const CoordType &b) {
+    T sum{0};
+    for (int i = 0; i < D; i++) {
+      T d = b[i] - a[i];
+      sum += d * d;
+    }
+    return sum;
+  }
+
+  static T euclideanDistance(const CoordType &a, const CoordType &b) {
+    return std::sqrt(euclideanReducedDistance(a, b));
+  }
+
+  T distanceReducedInternal(const CoordType &a, const CoordType &b) const {
+    switch (distanceType) {
+    case cmKDTreeDistanceEnum::CUSTOM:
+      return customReducedDistance(a, b);
+      break;
+    default:
+      return euclideanReducedDistance(a, b);
+    }
+  }
+
+  T distanceInternal(const CoordType &a, const CoordType &b) const {
+    switch (distanceType) {
+    case cmKDTreeDistanceEnum::CUSTOM:
+      return customDistance(a, b);
+    default:
+      return euclideanDistance(a, b);
+    }
+  }
+
+  std::pair<Node *, T> &traverseDown(Node *currentNode,
+                                     std::pair<Node *, T> &best,
+                                     const CoordType &x) const {
+    if (currentNode == nullptr)
+      return best;
+
+    int axis = currentNode->axis;
+
+    // For distance comparison operations we only use the "reduced" aka less
+    // compute intensive, but order preserving version of the distance
+    // function.
+    T distance = distanceReducedInternal(x, currentNode->value);
+    if (distance < best.second)
+      best = {currentNode, distance};
+
+    bool isLeft;
+    if (x[axis] < currentNode->value[axis]) {
+      best = traverseDown(currentNode->left, best, x);
+      isLeft = true;
+    } else {
+      best = traverseDown(currentNode->right, best, x);
+      isLeft = false;
+    }
+
+    // If the hypersphere with origin at x and a radius of our current best
+    // distance intersects the hyperplane defined by the partitioning of the
+    // current node, we also have to search the other subtree, since there could
+    // be points closer to x than our current best.
+    if (std::abs(x[axis] - currentNode->value[axis]) < best.second) {
+      if (isLeft)
+        best = traverseDown(currentNode->right, best, x);
+      else
+        best = traverseDown(currentNode->left, best, x);
+    }
+    return best;
   }
 
 public:
   cmKDTree() {}
-  cmKDTree(std::vector<PointType> &passedPoints)
-      : points(passedPoints), N(points.size()) {}
+
+  cmKDTree(lsSmartPointer<std::vector<CoordType>> passedPoints)
+      : N(passedPoints != nullptr ? passedPoints->size() : 0),
+        points(passedPoints) {}
 
   void build() {
-    if (points.size() == 0) {
-      lsMessage::getInstance()
-          .addWarning("No points have been provided.")
-          .print();
+    if (points == nullptr) {
+      std::cout << "No points available" << std::endl;
       return;
     }
-    std::vector<size_t> indices;
-    // Initialize the first index array with incrementing values
-    for (unsigned i = 0; i < N; i++)
-      indices.emplace_back(i);
 
-    //recurse(rootNode, indices, 0, 0);
+    if (rootNode != nullptr) {
+      std::cout << "Tree has already been built" << std::endl;
+      return;
+    }
 
-    /*
-    // Copy content of first vector to the others
-    indices[1] = indices[0];
-    if constexpr (D == 3)
-      indices[2] = indices[0];
+#pragma omp parallel default(none)                                             \
+    shared(numThreads, maxParallelDepth, surplusWorkers, std::cout, rootNode,  \
+           treeSize, points)
+    {
+#pragma omp single
+      {
+#ifdef _OPENMP
+        numThreads = omp_get_num_threads();
+#endif
+        maxParallelDepth = intLog2(numThreads);
+        surplusWorkers = numThreads - 1 << maxParallelDepth;
+#ifndef NDEBUG
+        std::cout << "Starting parallel region with " << numThreads
+                  << " parallel workers." << std::endl;
+#endif
 
-    // Sort the index vectors based on the coordinate value in each dimension
-    for (int i = 0; i < D; i++)
-      std::sort(indices[i].begin(), indices[i].end(), [&](size_t a, size_t b) {
-        return points[a][i] < points[b][i];
-      });
+        SizeType size = points->end() - points->begin();
+        SizeType medianIndex = (size + 1) / 2 - 1;
 
-    for (int i = 0; i < D; i++)
-      std::cout << "Index of first point in axis " << i << " is "
-                << indices[i][0] << std::endl;
+        std::nth_element(
+            points->begin(), points->begin() + medianIndex, points->end(),
+            [&](CoordType &a, CoordType &b) { return a[0] < b[0]; });
 
-    // Initialize index bounds of root node
-    rootNode.indexBounds = {0};
-    rootNode.items = N;
-    */
+        rootNode = new Node(*(points->begin() + medianIndex), 0);
+
+#ifdef _OPENMP
+        bool dontSpawnMoreThreads = 0 > maxParallelDepth + 1 ||
+                                    (0 == maxParallelDepth + 1 &&
+                                     omp_get_thread_num() >= surplusWorkers);
+#endif
+#pragma omp task final(dontSpawnMoreThreads)
+        {
+#ifdef _OPENMP
+#ifndef NDEBUG
+          std::cout << "Task assigned to thread " << omp_get_thread_num()
+                    << std::endl;
+#endif
+#endif
+          // Left Subtree
+          build(rootNode,                      // Use rootNode as parent
+                points->begin(),               // Data start
+                points->begin() + medianIndex, // Data end
+                1,                             // Depth
+                true                           // Left
+          );
+        }
+
+        // Right Subtree
+        build(rootNode,                          // Use rootNode as parent
+              points->begin() + medianIndex + 1, // Data start
+              points->end(),                     // Data end
+              1,                                 // Depth
+              false                              // Right
+        );
+#pragma omp taskwait
+      }
+    }
   }
 
-  void findNearestNeighbor(PointType &x) const {}
+  std::pair<CoordType, T> nearest(const CoordType &x) const {
+    auto best = std::pair{rootNode, std::numeric_limits<T>::infinity()};
+    best = traverseDown(rootNode, best, x);
+
+    return {best.first->value, distanceInternal(x, best.first->value)};
+  }
 
   ~cmKDTree() { recursiveFree(rootNode); }
 };
--- include/cmSurfaceMapper.hpp
+++ include/cmSurfaceMapper.hpp
@@ -1,22 +1,12 @@
 #ifndef CM_SURFACE_MAPPER_HPP
 #define CM_SURFACE_MAPPER_HPP
 
-//#include <vtkKdTreePointLocator.h>
-//#include <vtkOctreePointLocator.h>
-#include <vtkKdTree.h>
-#include <vtkNew.h>
-#include <vtkPoints.h>
-#include <vtkPolyData.h>
-
 #include <lsMesh.hpp>
 #include <lsMessage.hpp>
 #include <lsSmartPointer.hpp>
 
 #include "cmKDTree.hpp"
 
-// Finds the closest point on the base surface for each node on the advected
-// surface. The corresponding node index is stored in the advected surface's
-// scalar data.
 template <class T, int D> class cmSurfaceMapper {
   lsSmartPointer<lsMesh<>> baseSurface = nullptr;
   lsSmartPointer<lsMesh<>> advectedSurface = nullptr;
@@ -38,74 +28,26 @@ public:
       return;
     }
 
-    auto kdtree = lsSmartPointer<cmKDTree<T,D>>::New(baseSurface->nodes);
+    using CoordType = typename decltype(baseSurface->nodes)::value_type;
+
+    // Copy nodes to a new location
+    auto nodes = baseSurface->nodes;
+    auto points = lsSmartPointer<decltype(nodes)>::New(nodes);
+
+    auto kdtree = lsSmartPointer<cmKDTree<T, D, CoordType>>::New(points);
 
     kdtree->build();
 
-    /*
-    // Derived from the VTK KdTreePointLocatorClosestPoint example.
-    vtkNew<vtkPoints> points;
-    std::vector<double> IDs;
+    std::vector<double> layerThickness(advectedSurface->nodes.size(), -1);
 
-    for (int i = 0; i < baseSurface->nodes.size(); ++i) {
-      const auto &node = baseSurface->nodes[i];
-      points->InsertNextPoint(node[0], node[1], node[2]);
-      IDs.push_back(1.0 * i);
-    }
-
-    baseSurface->getCellData().insertNextScalarData(IDs, "Origin");
-
-    std::cout << points->GetNumberOfPoints() << std::endl;
-
-    vtkNew<vtkKdTree> tree;
-    if constexpr (D == 2) {
-      tree->OmitZPartitioning();
-    }
-    tree->SetMinCells(10);
-    tree->BuildLocatorFromPoints(points);
-
-    double bounds[6];
-    tree->GetBounds(bounds);
-
-    std::cout << "x: (" << bounds[0] << ", " << bounds[1] << ")\n"
-              << "y: (" << bounds[2] << ", " << bounds[3] << ")\n"
-              << "z: (" << bounds[4] << ", " << bounds[5] << ")\n";
-
-    T bmin = std::min({bounds[0], bounds[2], bounds[4]});
-    T bmax = std::max({bounds[1], bounds[3], bounds[5]});
-    T radius = std::sqrt(bmin * bmin + bmax * bmax);
-
-    std::vector<double> closestIDs(advectedSurface->nodes.size(), -1);
-
-    std::cout << advectedSurface->nodes.size() << ", "
-              << baseSurface->nodes.size() << std::endl;
-
-    const int numRegions = tree->GetNumberOfRegions();
-
-    // for (int i = 0; i < numRegions; i++) {
-    //
-    // }
-
-    tree->PrintTree();
-
-    //#pragma omp parallel for
     for (int i = 0; i < advectedSurface->nodes.size(); ++i) {
       const auto &node = advectedSurface->nodes[i];
-      double dist;
-      T tp[] = {node[0], node[1], node[2]};
-      vtkIdType iD = tree->FindClosestPoint(tp, dist);
-      // vtkIdType iD = tree->FindClosestPointWithinRadius(2., tp, dist);
-      const auto &baseNode = baseSurface->nodes[i];
-
-      // std::cout << "(" << node[0] << ", " << node[1] << ", " << node[2]
-      //           << ") -> (" << baseNode[0] << ", " << baseNode[1] << ", "
-      //           << baseNode[2] << "): " << dist << "\n";
-      // if (dist != 25)
-      closestIDs[i] = static_cast<double>(iD);
+      auto nearest = kdtree->nearest(node);
+      layerThickness[i] = nearest.second;
     }
 
-    advectedSurface->getCellData().insertNextScalarData(closestIDs, "Origin");
-    */
+    advectedSurface->getCellData().insertNextScalarData(layerThickness,
+                                                        "thickness");
   }
 };
 
