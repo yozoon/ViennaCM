71602d7c052744b78d437aaf4c77750ec50ca2fe
Julius Piso <julius@piso.at>
2022-03-04 12:39:59 +0100

    chamfer distance calculation uses omp for loop

--- include/cmMeshSimilarityScores.hpp
+++ include/cmMeshSimilarityScores.hpp
@@ -1,5 +1,5 @@
-#ifndef CM_POINT_CLOUD_SIMILARITY_SCORES_HPP
-#define CM_POINT_CLOUD_SIMILARITY_SCORES_HPP
+#ifndef CM_MESH_SIMILARITY_SCORES_HPP
+#define CM_MESH_SIMILARITY_SCORES_HPP
 
 #include <algorithm>
 #include <array>
@@ -10,15 +10,15 @@
 #include "cmKDTree.hpp"
 #include "cmPointLocator.hpp"
 
-template <class T, int D> struct cmPointCloudSimilarityScore {
-  cmPointCloudSimilarityScore() {}
-  cmPointCloudSimilarityScore(lsSmartPointer<lsMesh<>>) {}
+template <class T, int D> struct cmMeshSimilarityScore {
+  cmMeshSimilarityScore() {}
+  cmMeshSimilarityScore(lsSmartPointer<lsMesh<>>) {}
   virtual void setMesh(lsSmartPointer<lsMesh<>>) = 0;
   virtual T calculate(lsSmartPointer<lsMesh<>>) = 0;
 };
 
 template <class T, int D, class LocatorType = cmKDTree<std::array<T, 3>>>
-class cmChamferDistanceScore : cmPointCloudSimilarityScore<T, D> {
+class cmChamferDistanceScore : cmMeshSimilarityScore<T, D> {
   static_assert(
       std::is_base_of<cmPointLocator<std::array<T, 3>>, LocatorType>::value,
       "The passed point locator is not a subclass of cmPointLocator.");
@@ -56,10 +56,13 @@ public:
     secondLocator->build();
 
     T sum = 0.;
+
+#pragma omp parallel for default(shared) reduction(+ : sum)
     for (const auto &node : secondMesh->nodes) {
       sum += firstLocator->findNearest(node).second;
     }
 
+#pragma omp parallel for default(shared) reduction(+ : sum)
     for (const auto &node : firstMesh->nodes) {
       sum += secondLocator->findNearest(node).second;
     }
